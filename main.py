15b26b60ac1d128bb6ca5cfda6fa7f5cf7181181
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2025-11-07 13:20:52 +0100

    add new routes for login and register

--- main.py
+++ main.py
@@ -1,19 +1,91 @@
-import datetime as dt
-from flask import Flask,render_template, request, url_for
-from flask_cors import CORS
 import db
+import datetime as dt
+from flask import Flask,render_template, request, url_for, flash, redirect, session
+from flask_cors import CORS
+from models.users import login_manager, User, login_user, login_required
+from models.forms import RegisterForm, LoginForm, CSRFProtect
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+app.secret_key = db.SESSION_SECRET_KEY
+
+csrf = CSRFProtect(app)
 CORS(app)
+
+#session secret key
+
+
+#Set up Flask Login
+login_manager.init_app(app)
+login_manager.login_view ='view'
+
+
+
+@login_manager.user_loader # imported login_manager from the forms.py
+def load_user(user_id):
+    user_data = User.objects({'_id': user_id}).first()
+    if user_data:
+        return(User(user_data))
+    return 
+
+
+
+@app.route("/home")
+def homes():
+    return render_template("home.html") # renders the homepage
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm() # creates an instance of the loginform class
+    if form.validate_on_submit():
+        user = User.objects(email=form.email.data).first()  #checks for is any email is found in the database that matches the one provided by the user
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Logged in Successfully", 'success')
+            return redirect(url_for("/index"))
+        
+        else:
+            return flash("Invalid Email or Password. Try again!", 'warning')
+    return render_template('login.html', form=form)
+
+
+#Register route
+@app.route("/signup", methods=["GET", "POST"])
+def register():
+    form = RegisterForm() # calls the Register WTForm from forms.py file
+    if form.validate_on_submit():
+        existing_user = User.objects({'email':form.email.data}).first() # checks if the email already exist in the database
+
+        if existing_user:
+            flash("Email already Registered. Please Login or try another email", 'warning') # shows an alert fo successfully registering!
+            return redirect(url_for("register"))
+        
+        hash_password = generate_password_hash(form.password.data) # hashes the password of the user
+        users = User(username=form.username.data,
+                      email=form.email.data,
+                        password=hash_password)
+        
+        users.save() # stores the provided data to the database
+
+        flash("Registration Successful, Plese Login")
+        return redirect(url_for('login')) # redirects the the Login page!
+   
+    return render_template('register.html', form=form)
+
+
+
+
+
+
 
 today = dt.datetime.today()
 # gets the data of every registered user including name and date of birth etc.
 
-
-
 # This is the home page route to to show the active Birthdays 
-@app.route('/',methods=["GET", "POST"])
+@login_required
+@app.route('/index',methods=["GET", "POST"])
 def home():
     saved_birthdays= db.DateOfBirth.objects().all()
 
@@ -59,6 +131,7 @@ def home():
 
 
 #this is the route that the javascript fetch function listens to, to post the form data to the database
+@login_required
 @app.route('/submit',methods=[ "POST"])
 def submit():
      if len(request.form) > 0:   
